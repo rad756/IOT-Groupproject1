93e7d851a4b2127022439c5599138ada4dd3651e
Endadru98 <36032808+Endadru98@users.noreply.github.com>
2018-03-22 10:49:27 +0000

    Add files via upload

---
+++ lightsensorandlcdworking.ino
@@ -0,0 +1,39 @@
+
+#include <Wire.h>
+#include "rgb_lcd.h"
+#include <math.h>
+
+rgb_lcd lcd;
+
+const int colorR = 255;
+const int colorG = 0;
+const int colorB = 0;
+const int sensor=A2; 
+
+void setup() 
+{
+    // set up the LCD's number of columns and rows:
+    lcd.begin(16, 2);
+    
+    lcd.setRGB(colorR, colorG, colorB);
+    
+
+    Serial.begin(9600);
+}
+
+void loop() 
+{
+    // set the cursor to column 0, line 1
+    // (note: line 1 is the second row, since counting begins with 0):
+    lcd.setCursor(0, 1);
+  if(analogRead(A1) > 300)
+  {
+    lcd.print("You got mail!");
+  }
+
+    delay(1000);
+      int sensorValue=analogRead(sensor);
+  Serial.println("the analog read data is ");
+  Serial.println(sensorValue);
+  delay(1000);
+}
